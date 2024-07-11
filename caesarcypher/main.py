be15a4385583a5f09de14fb7f1a6f155bf38205f
thejokeson-you <milansaleh2004@gmail.com>
2024-07-11 21:06:29 +0100

    Implement encryption method

--- caesarcypher/main.py
+++ caesarcypher/main.py
@@ -1,11 +1,27 @@
 # This is a sample Python script.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t"
+    , "u", "v", "w", "x", "y", "z"]
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def encrypt(inpstr, key):
+    inp_list = list(inpstr)
+    new_inp_list = []
+    i = 0
+    for x in inp_list:
+        if x == " ":
+            new_inp_list[i] == " "
+        else:
+            new_index = (alphabet.index(x) + key) % 26
+            new_inp_list.append(alphabet[new_index])
+
+        i += 1
+
+    return str(new_inp_list)
+
+
+print("CAESAR CYPHER")
+print("----------------------")
+inpString = input("Enter the string you want to encrypt: ")
+inpKey = int(input("Enter the key which you want to shift by: "))
+encrypt(inpString, inpKey)
